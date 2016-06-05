91753eb911346e6bf531c3c628d2675bcbbbfc14
Kathleen L <kathleeng.lopez@gmail.com>
2016-06-04 17:31:13 -0700

    Working through sum lists

--- Chapter 2 - Linked Lists/2.4-Partition.js
+++ Chapter 2 - Linked Lists/2.4-Partition.js
@@ -71,3 +71,85 @@ console.log('partition', partition(linkedlist, 5) );
 
 
 
+//////////////////////////
+//Creating a Linked List//
+//////////////////////////
+function Node ( value ) {
+  var node = {};
+  node.value = value;
+  node.next = null;
+  return node;
+};
+
+function LinkedList () {
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+//Add a node to the end of a linked list
+  list.addToTail = function(value) {
+    var newTail = Node(value);
+
+    if ( !list.head ) {
+      list.head = newTail;
+    }
+
+    if ( list.tail ) {
+      list.tail.next = newTail;
+    }
+
+    list.tail = newTail;
+  
+  };
+
+//Remove the first node
+  list.removeHead = function() {
+    if (list.head === null) {
+      return null;
+    }
+
+    var currentHead = list.head;
+    list.head = list.head.next;
+    return currentHead.value;
+
+  };
+
+  list.contains = function(target) {
+    var node = list.head;
+
+    while (node) {
+      if (node.value === target) {
+        return true;
+      }
+      node = node.next;
+    } 
+    return false;
+  };
+
+//Deleting a Node from a Singly Linked List
+  list.deleteNode = function ( target ) {
+    var node = list.head;
+
+    if ( node.value === target ) {
+      return list.head.next;
+    }
+
+    while ( node.next !== null ) {
+      if ( node.next.value === target ) {
+        node.next = node.next.next;
+        return list.head;
+      }
+      node = node.next;
+    }
+  };
+  
+  return list;
+};
+
+var linkedlist = new LinkedList();
+linkedlist.addToTail(1);
+linkedlist.addToTail(2);
+linkedlist.addToTail(3);
+linkedlist.addToTail(4);
+console.log(linkedlist);
+console.log(linkedlist.deleteNode(2));
---
+++ Chapter 2 - Linked Lists/2.5-SumLists.js
@@ -0,0 +1,125 @@
+//2.5 Sum Lists: You have two numbers represented by a linked list, where each node contains a single digit. the digits are stored in reverse order, such that the 1's digit is at the head of the list. Write a function that adds the two numbers and returns the sum as a linked list.
+
+/*
+input: (7->1->6) + (5->9->2). //617 + 295
+output: (2->1->9) //912
+*/
+
+//function should take in 2 lists, and a carry variable
+var sumLists = function ( list1, list2, carry ) {
+  //check if the input items are valid
+  if ( list1 === null || list2 === null || carry === 0) {
+    return null;
+  }
+
+  //create a results Node
+  var result = new Node();
+
+  //variable to hold carry
+  num = carry;
+  //
+  if ( list1 !== null ) {
+    num += list1.value;
+  } 
+  if ( list2 !== null ) {
+    num += list2.value;
+  }
+
+  result.value = num % 10;
+
+  if ( list1 !== null || list2 !== null ) {
+    var node = addLists( list1 == null ? null : list1.next, list2 == null ? null : list2.next, num >= 10 ? 1 : 0 );
+    result.next(node);
+  }
+
+  return list;
+};
+
+//////////////////////////
+//Creating a Linked List//
+//////////////////////////
+function Node ( value ) {
+  var node = {};
+  node.value = value;
+  node.next = null;
+  return node;
+};
+
+function LinkedList () {
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+//Add a node to the end of a linked list
+  list.addToTail = function(value) {
+    var newTail = Node(value);
+
+    if ( !list.head ) {
+      list.head = newTail;
+    }
+
+    if ( list.tail ) {
+      list.tail.next = newTail;
+    }
+
+    list.tail = newTail;
+  
+  };
+
+//Remove the first node
+  list.removeHead = function() {
+    if (list.head === null) {
+      return null;
+    }
+
+    var currentHead = list.head;
+    list.head = list.head.next;
+    return currentHead.value;
+
+  };
+
+  list.contains = function(target) {
+    var node = list.head;
+
+    while (node) {
+      if (node.value === target) {
+        return true;
+      }
+      node = node.next;
+    } 
+    return false;
+  };
+
+//Deleting a Node from a Singly Linked List
+  list.deleteNode = function ( target ) {
+
+    var node = list.head;
+
+    if ( node.value === target ) {
+      return list.head.next;
+    }
+
+    while ( node.next !== null ) {
+      if ( node.next.value === target ) {
+        node.next = node.next.next;
+        return list.head;
+      }
+      node = node.next;
+    }
+  };
+  
+  return list;
+};
+
+var linkedlist1 = new LinkedList();
+var linkedlist2 = new LinkedList();
+linkedlist1.addToTail(7);
+linkedlist1.addToTail(1);
+linkedlist1.addToTail(6);
+linkedlist2.addToTail(3);
+linkedlist2.addToTail(9);
+linkedlist2.addToTail(2);
+console.log(linkedlist1);
+console.log(linkedlist2);
+
+
