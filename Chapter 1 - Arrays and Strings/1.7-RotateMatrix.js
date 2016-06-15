2e2ab4eb299aadbed3ef26b8dfb658785e6abdea
Kathleen L <kathleeng.lopez@gmail.com>
2016-06-14 21:50:54 -0700

    dummy matrix added

---
+++ Chapter 1 - Arrays and Strings/1.7-RotateMatrix.js
@@ -0,0 +1,10 @@
+//1.7 Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+
+    m = [
+      [1, 0, 0, 0],
+      [0, 1, 0, 0],
+      [0, 0, 1, 0],
+      [0, 0, 0, 1]
+    ];
+
+console.log(rotateMatrix(m));
