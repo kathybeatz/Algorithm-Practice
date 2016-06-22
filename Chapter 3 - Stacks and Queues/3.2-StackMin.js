3e34fc33465d58de9aaea3803de8533c544964e0
Kathleen L <kathleeng.lopez@gmail.com>
2016-06-21 19:25:12 -0700

    Added Stack Min Solution

--- Chapter 3 - Stacks and Queues/3.2-StackMin.js
+++ Chapter 3 - Stacks and Queues/3.2-StackMin.js
@@ -8,4 +8,40 @@
  */
 
  //Solution 1:
- 
+var MinStack = function() {
+  this._stack = [];
+};
+
+MinStack.prototype.push = function() {
+  var min = this.min();
+  this._stack.push({
+    value: value,
+    min: Math.min(min !== undefined ? min : Number.POSITIVE_INFINITY, value);
+  });
+};
+
+MinStack.prototype.pop = function() {
+  if (!this.isEmpty()) {
+    var item = this._stack.pop();
+    return item.value;
+  }
+};
+
+MinStack.prototype.peek = function() {
+  if (!this.isEmpty()) {
+    var item = this._stack[this._stack.length - 1];
+    return item.value;
+  }
+};
+
+MinStack.prototype.min = function() {
+  if (!this.isEmpty()) {
+    var item = this._stack[this._stack.length - 1];
+    return item.min;
+  }
+};
+
+MinStack.prototype.isEmpty = function() {
+  return this._stack.length === 0;
+};
+
