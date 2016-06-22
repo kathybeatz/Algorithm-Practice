dadabf352ba4faf91917d60b6e9054480e1d959c
Kathleen L <kathleeng.lopez@gmail.com>
2016-06-21 17:12:15 -0700

    Added Three in One pseudoclassical solution

--- Chapter 3 - Stacks and Queues/3.1-ThreeInOne.js
+++ Chapter 3 - Stacks and Queues/3.1-ThreeInOne.js
@@ -1,44 +1,66 @@
 //3.1 Three in One: Describe how you could use a single array to implement three stacks.
 
-var TripleStack = function () {
-  this.array = [];
-  this.lengths = [0,0,0];
+
+//Solution 1: Pseudoclassical TripleStack implementation
+var TripleStack = function() {
+  this._array = [];
+  this._lengths = [0, 0, 0];
+  };
+
+TripleStack.prototype.getLength = function(stack) {
+  return this._lengths[stack - 1];
 };
 
-
-//////////////////////////
-//   Creating a Stack   //
-//////////////////////////
-
-var Stack = function() {
-  this._storage = {};
-  this._size = 0;
+TripleStack.prototype.push = function(stack, value) {
+  var index = (this.getLength(stack) * 3) + stack - 1;
+  console.log('pushed index', index);
+  this._array[index] = value;
+  console.log('value', this._array[index]);
+  this._lengths[stack - 1]++;
 };
 
-
-Stack.prototype.push = function(value){
-  this._storage[this._size] = value;
-  this._size++;
+TripleStack.prototype.pop = function(stack) {
+  var length = this.getLength(stack);
+  var value;
+  if (length > 0) {
+    var index = (length - 1) * 3 + stack - 1;
+    value = this._array[index];
+    delete this._array[index] 
+    this._lengths[stack - 1]--;
+  }
+  return value;
 };
 
-Stack.prototype.pop = function(){
-  this._size && this._size--;
-  var result = this._storage[this._size];
-
-  delete this._storage[this._size];
-
-  return result;
+//return the top of the stack
+TripleStack.prototype.peek = function(stack) {
+  var length = this.getLength(stack);
+  var value;
+  if (length > 0) {
+    var index = (length - 1) * 3 + stack - 1;
+    value = this._array[index];
+  }
+  return value;
 };
 
-Stack.prototype.size = function(){
-  return this._size;
+TripleStack.prototype.isEmpty = function(stack) {
+  return this.getLength(stack) === 0;
 };
 
+var threeStacks = new TripleStack();
 
-var stackz = new Stack();
+threeStacks.push(1, 4);
+threeStacks.push(1, 5);
+threeStacks.push(1, 6);
+threeStacks.push(2, 7);
+threeStacks.push(2, 8);
+threeStacks.push(2, 9);
+threeStacks.push(3, 1);
+threeStacks.push(3, 2);
+threeStacks.push(3, 3);
+console.log(threeStacks);
+console.log('peek stack 1', threeStacks.peek(1)); //should be 6
+console.log('peek stack 2', threeStacks.peek(2)); //should be 9
+console.log('peek stack 3', threeStacks.peek(3)); //should be 3
+threeStacks.pop(1);
+console.log(threeStacks);
 
-stackz.push(1);
-stackz.push(2);
-console.log(stackz)
-stackz.pop();
-console.log(stackz)
