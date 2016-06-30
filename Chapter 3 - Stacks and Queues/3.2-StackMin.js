5d36ad832faa68c65fbb35234f46a5e21a0053a8
Kathleen L <kathleeng.lopez@gmail.com>
2016-06-30 15:58:45 -0700

    Added pseudocode for Min Stack

--- Chapter 3 - Stacks and Queues/3.2-StackMin.js
+++ Chapter 3 - Stacks and Queues/3.2-StackMin.js
@@ -8,36 +8,47 @@
  * that point.
  */
 
- //Solution 1:
+ //Solution 1: Pseudoclassical implementation
 var MinStack = function() {
   this._stack = [];
 };
 
-MinStack.prototype.push = function() {
+
+MinStack.prototype.push = function(value) {
+  //grab the current min
   var min = this.min();
+  //push an object into the stack containing
   this._stack.push({
+    //the value 
     value: value,
-    min: Math.min(min !== undefined ? min : Number.POSITIVE_INFINITY, value);
+    //check the min against the value pushed in, set the new min if value is less than the current min
+    min: Math.min(min !== undefined ? min : Number.POSITIVE_INFINITY, value)
   });
 };
 
 MinStack.prototype.pop = function() {
   if (!this.isEmpty()) {
+    //use native Array pop to remove top item on stack
     var item = this._stack.pop();
+    //return the value of the item on the top of the stack
     return item.value;
   }
 };
 
 MinStack.prototype.peek = function() {
   if (!this.isEmpty()) {
+    //get the item on the top of the stack
     var item = this._stack[this._stack.length - 1];
+    //return the value of that item
     return item.value;
   }
 };
 
 MinStack.prototype.min = function() {
   if (!this.isEmpty()) {
+    //grab the top item in the stack
     var item = this._stack[this._stack.length - 1];
+    //grab the current min
     return item.min;
   }
 };
@@ -46,3 +57,13 @@ MinStack.prototype.isEmpty = function() {
   return this._stack.length === 0;
 };
 
+var minimumStack = new MinStack();
+minimumStack.push(5);
+minimumStack.push(3);
+minimumStack.push(6);
+minimumStack.push(2);
+minimumStack.push(4);
+console.log(minimumStack);
+minimumStack.pop();
+minimumStack.pop();
+console.log('after pop', minimumStack);
