8ebc872b432fe77ab1cc17db87618135c16ad6b7
Kathleen L <kathleeng.lopez@gmail.com>
2016-07-01 01:15:25 -0700

    Added solution to sort stack, working on pseudocode

--- Chapter 3 - Stacks and Queues/3.5-SortStack.js
+++ Chapter 3 - Stacks and Queues/3.5-SortStack.js
@@ -9,3 +9,44 @@
 
 
 //Solution 1:
+
+//function takes in a stack
+var sortStack = function(stack) {
+  //create temporary stack
+  var tempStack = [];
+  //put the last item of the stack in to the temp stack
+  tempStack.push(stack.pop());
+  
+  //loop as long as the stack is not empty
+  while (!isEmpty(stack)) {
+    //set current equal to the last item in the temporary stack
+    var current = tempStack.pop();
+    //set count to 0
+    var count = 0;
+
+    //loop as long as the temporary stack is not empty and the current item is less than the last item in the temporary stack
+    while (!isEmpty(temp) && current < peek(tempStack)) {
+      //
+      stack.push(tempStack.pop());
+      count++;
+    }
+    tempStack.push(current);
+    for (var i = 0; i < count; i++) {
+      tempStack.push(stack.pop());
+    }
+  }
+
+  while (!isEmpty(temp)) {
+    stack.push(tempStack.pop());
+  }
+
+  return stack;
+};
+
+var peek = function(stack) {
+  return stack[stack.length - 1];
+};
+
+var isEmpty = function(stack) {
+  return stack.length === 0;
+};
