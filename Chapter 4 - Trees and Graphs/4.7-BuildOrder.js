dfb05db7b2bbb65253ee3106eb426233fc562a4c
Kathleen L <kathleeng.lopez@gmail.com>
2016-07-22 13:23:33 -0700

    Added Build Order

---
+++ Chapter 4 - Trees and Graphs/4.7-BuildOrder.js
@@ -0,0 +1,9 @@
+//4.7 Build Order: You are given a list of projects and a list of dependencies (which is a list of pairs of projects, where the first project is dependent on the second project). All of a project's dependencies must be built before the project is. Find a build order that will allow the projects to be built. If there is no valid build order, return an error.
+
+//EXAMPLE
+//Input:
+// projects: a, b, c, d, e, f
+// dependencies: (d, a), (b, f), (d, b), (a, f), (c, d)
+//Output: f, e, a, b, d, c
+
+//Solution 1:
