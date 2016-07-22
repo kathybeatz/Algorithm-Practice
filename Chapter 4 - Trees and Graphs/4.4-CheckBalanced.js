9e19d213d3a0f7462e8e7ade30d739adda12f7d1
Kathleen L <kathleeng.lopez@gmail.com>
2016-07-22 13:21:59 -0700

    Added Check Balanced

---
+++ Chapter 4 - Trees and Graphs/4.4-CheckBalanced.js
@@ -0,0 +1,3 @@
+//4.4 Check Balanced: Implement a function to check if a binary tree is balanced. For the purpose of this question, a balanced tree is defined to be a tree such that the heights of the two subtrees of any node never differe by more than one.
+
+//Solution 1:
